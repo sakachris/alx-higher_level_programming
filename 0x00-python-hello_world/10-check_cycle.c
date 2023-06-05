9a24d795de680804e9e79ca0d5a1dc92b18cd912
sakachris <sakachris90@gmail.com>
2023-06-05 22:27:24 +0000

    set variables to point differently

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,8 +9,8 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *slow = list->next;
+	listint_t *fast = list->next->next;
 
 	if (!list)
 		return (0);
