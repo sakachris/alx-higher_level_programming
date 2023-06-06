e7a8444e519e8b29189f6fa2676b757474741ae8
sakachris <sakachris90@gmail.com>
2023-06-06 13:16:03 +0000

    fetch and print last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# fetching the last number
+if number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
+
+# printing the number and last number
+if last_digit > 5:
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is 0")
+else:
+    print("Last digit of", number, "is", last_digit,
+          "and is less than 6 and not 0")
