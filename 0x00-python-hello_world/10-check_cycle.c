b00880f3c228ebef60f66c34bdd4f6dec0b87d4f
sakachris <sakachris90@gmail.com>
2023-06-07 12:30:19 +0000

    fixed case when head is null

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,11 +9,14 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list->next;
+	listint_t *slow;
+	listint_t *fast;
 
-	if (!list)
+	if (list == NULL)
 		return (0);
+
+	slow = list;
+	fast = list->next;
 
 	while (slow && fast && fast->next)
 	{
