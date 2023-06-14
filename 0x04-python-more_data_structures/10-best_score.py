d47442793ba3eca6c8fa7efba38aba6ed9fce9c8
sakachris <sakachris90@gmail.com>
2023-06-14 14:06:55 +0000

    fixing extra spaces

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return
-    k = max(a_dictionary, key = lambda i: a_dictionary[i])
+    k = max(a_dictionary, key=lambda i: a_dictionary[i])
     return k
