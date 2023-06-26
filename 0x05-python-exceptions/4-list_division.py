6afe3b5268e03dfd4cfb9811ce2143201c870dea
sakachris <sakachris90@gmail.com>
2023-06-26 14:48:11 +0000

    adding finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,5 +14,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             quo.append(0)
             print("out of range")
+        finally:
+            pass
     return quo
 
