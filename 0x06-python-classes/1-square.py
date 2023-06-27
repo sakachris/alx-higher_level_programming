0f5e45d7e2568e735e483602e1f002d8c1eccec9
sakachris <sakachris90@gmail.com>
2023-06-27 12:39:47 +0000

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Class Square """
+
+
+class Square:
+    """ Initializing Square
+    Args:
+        __size: Private size of square
+    """
+    def __init__(self, size):
+        self.__size == size
