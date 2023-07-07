700d0fd82866464c1b6292b53328c1a234741717
sakachris <sakachris90@gmail.com>
2023-07-07 07:26:00 +0000

    float(inf)

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(float('NaN'), 2))
+print(add_integer(float('inf'), float('inf')))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
