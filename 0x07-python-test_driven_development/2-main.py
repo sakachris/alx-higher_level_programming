56415d4b42a80fabcd7bae5960fec44c91c0d04b
sakachris <sakachris90@gmail.com>
2023-07-07 08:02:46 +0000

    addding test for no argument and 1 argument

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-#matrix = [
-#[1, 2, 3],
-#[4, 5, 6]
-#]
+matrix = [
+[1, 2, 3],
+[4, 5, 6]
+]
 
-matrix = [[2,3,8],[4,5,10],(2,3,4)]
-print(matrix_divided(matrix, 3))
+#matrix = [[2,3,8],[4,5,10],(2,3,4)]
+print(matrix_divided())
 print(matrix)
