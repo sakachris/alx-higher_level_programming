32b855b0af2a2a5ddf4c165891ec65520d80852b
sakachris <sakachris90@gmail.com>
2023-07-10 12:30:26 +0000

    issubclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module for a function that tests for a subclass
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    checks if the object is a subclass of the specified class
+    """
+    if type(obj) != a_class:
+        return issubclass(type(obj), a_class)
