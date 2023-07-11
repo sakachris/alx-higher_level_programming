555acfaabeddd0fb6e03fcb5506f96e187c2b2c4
sakachris <sakachris90@gmail.com>
2023-07-11 09:16:08 +0000

    reading file2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     reads a text file (UTF8) and prints it to stdout
     """
 
-    with open(filename, encoding='utf-8') as file:
-        print(file.read().rstrip())
+    with open(filename, encoding='UTF-8') as file:
+        print(file.read(), end="")
