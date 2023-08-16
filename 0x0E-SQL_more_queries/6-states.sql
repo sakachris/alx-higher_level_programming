5b3999da61a4850f160ffb3cbb7bfcf4aa87ccff
sakachris <sakachris90@gmail.com>
2023-08-16 23:03:48 +0300

    states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_usa and the table states
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- name VARCHAR(256) can’t be null
+
+CREATE TABLE IF NOT EXISTS unique_id (
+    id AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE(id)
+);
