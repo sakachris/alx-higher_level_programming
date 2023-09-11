a3cbe67aefb46b5ebad7ed6c3aac47e5aa38c947
sakachris <sakachris90@gmail.com>
2023-09-12 00:52:16 +0300

    fixing sort in task 12

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,14 @@
 
 const myVar = process.argv.length;
 if (myVar < 4) {
-  console.log('0');
+  console.log(0);
 } else {
   let array = [];
   for (let i = 2; i < process.argv.length; i++) {
     array.push(parseInt(process.argv[i]));
-    array = array.sort();
+    array = array.sort(function(a, b) {
+      return a - b;
+    })
   }
   console.log(array[array.length - 2]);
 }
