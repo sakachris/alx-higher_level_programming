0928705533024c273d6174f60d4c4787fc40f023
sakachris <sakachris90@gmail.com>
2023-09-12 00:55:05 +0300

    fixing task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,9 +7,9 @@ if (myVar < 4) {
   let array = [];
   for (let i = 2; i < process.argv.length; i++) {
     array.push(parseInt(process.argv[i]));
-    array = array.sort(function(a, b) {
+    array = array.sort(function (a, b) {
       return a - b;
-    })
+    });
   }
   console.log(array[array.length - 2]);
 }
