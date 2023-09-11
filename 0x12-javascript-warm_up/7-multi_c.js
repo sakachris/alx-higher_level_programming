b23ca82af2bd2554a273467827f34a2126749d78
sakachris <sakachris90@gmail.com>
2023-09-12 00:35:17 +0300

    task 7 error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const myVar1 = process.argv[2];
 const myVar2 = parseInt(myVar1, 10);
 if (myVar2) {
   for (let i = 0; i < myVar2; i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
