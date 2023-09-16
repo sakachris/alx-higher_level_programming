76cc6f860fc5df85ae03a96ec1e8c5af9bc2ccf3
sakachris <sakachris90@gmail.com>
2023-09-17 00:28:31 +0300

    first state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module for first state model
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Inheriting base class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
