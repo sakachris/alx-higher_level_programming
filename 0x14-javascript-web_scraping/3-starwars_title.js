9aa765a2f68a63f2c8a5433efe0ffbd9f6da42b5
sakachris <sakachris90@gmail.com>
2023-10-24 17:36:42 +0300

    star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    const movies = JSON.parse(body);
+    console.log(movies.title);
+  }
+});
